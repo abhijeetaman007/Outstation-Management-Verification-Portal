816651835991d33ca1e96f2f4b6738b61b433de5
ArjunPraveen <arjun.praveen@learner.manipal.edu>
2020-10-27 11:18:11 +0400

    sort by date and verified

--- OM_Portal_final/routes/index.js
+++ OM_Portal_final/routes/index.js
@@ -15,12 +15,25 @@ router.get('/users/dashboard',ensureAuthenticated,(req,res)=>res.render('dashboa
 //Admin Dashboard
 router.get('/users/admindashboard',ensureAuthenticated,(req,res)=>{
     // console.log(req.user.name)
-    mongoose.model("User").find({role:'User'},(err,users)=>{
+    compare = (a,b) => {
+        let priority = {
+            'UNVERIFIED' : 0,
+            'VERIFIED' : 1,
+            'REJECTED' : 2
+        }
+        if(priority[a.verified] > priority[b.verified])
+            return 1;
+        else if(priority[a.verified] < priority[b.verified])
+            return -1;
+        else
+            return 0
+    } 
+    mongoose.model("User").find({role:'User'},{},{sort:{timeStamp:1}},(err,users)=>{
         if(err) throw err
         else
-        {
+        {   
             res.render('admindashboard',{
-                users:users
+                users:users.sort(compare)
             })
         }
     })
